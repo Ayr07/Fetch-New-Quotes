2bfcd9856ffdeb0c69ede03cfd406e38ef492409
Ayaan RIzvi <132639354+Ayr07@users.noreply.github.com>
2024-01-08 05:09:32 +0530

    Add files via upload

---
+++ Fetch Quotes/fetchQuotes.js
@@ -0,0 +1,12 @@
+fetchQuote = ()=>{
+    fetch('https://type.fit/api/quotes').then((data) =>{
+        return data.json();
+    })
+    .then((data)=>{
+        let random = Math.floor(Math.random()*data.length);
+        document.getElementById('quote').innerText = data[random].text;
+        document.getElementById('author').innerText = data[random].author
+    })
+}
+
+fetchQuote()
